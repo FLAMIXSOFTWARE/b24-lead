37cfcc5d46284c22e484a0614708fbcf5b24e852
Roman Shkabko <r.shkabko@flamix.email>
2023-08-05 11:24:55 +0300

    Lead.php: session start && Small refactoring

--- src/Bitrix24/Lead.php
+++ src/Bitrix24/Lead.php
@@ -2,7 +2,10 @@
 
 namespace Flamix\Bitrix24;
 
-use \GuzzleHttp\Client as Http;
+use Flamix\Conversions\Conversion;
+use UtmCookie\UtmCookie;
+use GuzzleHttp\Client as Http;
+use Exception;
 
 class Lead
 {
@@ -14,87 +17,112 @@ class Lead
     private static $version = 'v1';
     private static $send_prepared_anal_data = true;
 
-
-    protected function __construct() {}
-    protected function __clone() {}
-    public function __wakeup()
+    protected function __construct()
     {
-        throw new \Exception("Cannot unserialize a singleton.");
     }
 
-    public static function getInstance(): Lead
+    protected function __clone()
     {
-        if( empty(self::$instances) )
-            self::$instances = new static;
+    }
 
+    public function __wakeup()
+    {
+        throw new Exception("Cannot unserialize a singleton.");
+    }
+
+    public static function getInstance(): self
+    {
+        if (empty(self::$instances)) {
+            self::$instances = new static;
+        }
+
+        self::session();
         return self::$instances;
     }
 
     /**
-     * Set your API code
+     * Ensure that the session starts. If not started - run!
+     *
+     * @return bool
+     */
+    private static function session(): bool
+    {
+        try {
+            if (session_status() === PHP_SESSION_NONE) {
+                session_start();
+            }
+
+            return true;
+        } catch (Exception $e) {
+            return false;
+        }
+    }
+
+    /**
+     * Set your API code.
      *
      * @param string $token
-     * @return mixed
+     * @return self
      */
-    public static function setToken(string $token)
+    public static function setToken(string $token): self
     {
         self::$token = $token;
         return self::$instances;
     }
 
     /**
-     * Set your DOMAIN (for auth)
+     * Set your DOMAIN (for auth).
      *
      * @param string $domain
-     * @return mixed
+     * @return self
      */
-    public static function setDomain(string $domain)
+    public static function setDomain(string $domain): self
     {
         self::$domain = $domain;
         return self::$instances;
     }
 
     /**
-     * Set extra fields to Lead
+     * Set extra fields to Lead.
      *
      * @param array $fields
-     * @return mixed
+     * @return self
      */
-    public static function setExtraFields(array $fields)
+    public static function setExtraFields(array $fields): self
     {
         self::$extra_fields = $fields;
         return self::$instances;
     }
 
     /**
-     * Change SubDomain if we have APP on unother portal
+     * Change SubDomain if we have APP on another portal.
      *
      * @param string $subdomain
-     * @return mixed
+     * @return self
      */
-    public static function changeSubDomain(string $subdomain)
+    public static function changeSubDomain(string $subdomain): self
     {
         self::$subdomain = $subdomain;
         return self::$instances;
     }
 
     /**
-     * Disable auto get trace, host, IP etc
+     * Disable auto get trace, host, IP, etc.
      *
-     * @return mixed
+     * @return self
      */
-    public static function disableAutoAnalytics()
+    public static function disableAutoAnalytics(): self
     {
         self::$send_prepared_anal_data = false;
         return self::$instances;
     }
 
     /**
-     * Enable auto get trace, host, IP etc
+     * Enable auto-get trace, host, IP, etc.
      *
-     * @return mixed
+     * @return self
      */
-    public static function enableAutoAnalytics()
+    public static function enableAutoAnalytics(): self
     {
         self::$send_prepared_anal_data = true;
         return self::$instances;
@@ -105,7 +133,7 @@ class Lead
      *
      * @return string
      */
-    public static function getURL()
+    public static function getURL(): string
     {
         return 'https://' . self::$subdomain . self::$url . self::$version . '/';
     }
@@ -117,32 +145,39 @@ class Lead
      */
     private static function prepareAnalyticsData(&$data)
     {
-        if(empty($data['UF_CRM_FX_CONVERSION']))
-            $data['UF_CRM_FX_CONVERSION'] = \Flamix\Conversions\Conversion::getPreparedUID();
+        if (empty($data['UF_CRM_FX_CONVERSION'])) {
+            $data['UF_CRM_FX_CONVERSION'] = Conversion::getPreparedUID();
+        }
 
-        //Add UTM if PHP is good
-        if(empty($data['UTM']) && version_compare(PHP_VERSION, '7.2.0') >= 0)
-            $data['UTM'] = \UtmCookie\UtmCookie::get();
+        // Add UTM if PHP is good
+        if (empty($data['UTM']) && version_compare(PHP_VERSION, '7.2.0') >= 0) {
+            $data['UTM'] = UtmCookie::get();
+        }
 
-        //TRACE
-        if(empty($data['TRACE']))
+        // TRACE
+        if (empty($data['TRACE'])) {
             $data['TRACE'] = Trace::get(true);
+        }
 
         //HOSTNAME
-        if(empty($data['FIELDS']['HOSTNAME']) && !empty(SmartUTM::getMyHostname()))
+        if (empty($data['FIELDS']['HOSTNAME']) && !empty(SmartUTM::getMyHostname())) {
             $data['FIELDS']['HOSTNAME'] = SmartUTM::getMyHostname();
+        }
 
         //REFERER
-        if(empty($data['FIELDS']['REFERER']) && !empty(SmartUTM::getReferer()))
+        if (empty($data['FIELDS']['REFERER']) && !empty(SmartUTM::getReferer())) {
             $data['FIELDS']['REFERER'] = SmartUTM::getReferer();
+        }
 
         //USER IP
-        if(empty($data['FIELDS']['USER_IP']) && !empty(SmartUTM::getMyIP()))
+        if (empty($data['FIELDS']['USER_IP']) && !empty(SmartUTM::getMyIP())) {
             $data['FIELDS']['USER_IP'] = SmartUTM::getMyIP();
+        }
 
         //ROISTAT_VISIT_ID
-        if(empty($data['FIELDS']['ROISTAT_VISIT_ID']) && !empty(SmartUTM::getRoistatID()))
+        if (empty($data['FIELDS']['ROISTAT_VISIT_ID']) && !empty(SmartUTM::getRoistatID())) {
             $data['FIELDS']['ROISTAT_VISIT_ID'] = SmartUTM::getRoistatID();
+        }
     }
 
     /**
@@ -150,23 +185,26 @@ class Lead
      *
      * @param array $data
      * @return array
-     * @throws \Exception
+     * @throws Exception
      */
-    public static function prepareData(array $data = [])
+    public static function prepareData(array $data = []): array
     {
-        if(empty(self::$domain))
-            throw new \Exception('Empty DOMAIN!');
+        if (empty(self::$domain)) {
+            throw new Exception('Empty DOMAIN!');
+        }
 
-        if(empty(self::$token))
-            throw new \Exception('Empty api_token!');
+        if (empty(self::$token)) {
+            throw new Exception('Empty api_token!');
+        }
 
         $data = array_merge($data, [
             'DOMAIN' => self::$domain,
             'api_token' => self::$token,
         ]);
 
-        if(self::$send_prepared_anal_data)
+        if (self::$send_prepared_anal_data) {
             self::prepareAnalyticsData($data);
+        }
 
         return $data;
     }
@@ -175,7 +213,7 @@ class Lead
      * @param array $data
      * @param string $actions
      * @return mixed
-     * @throws \Exception
+     * @throws Exception
      */
     public static function send(array $data = [], string $actions = 'lead/add')
     {
@@ -189,8 +227,9 @@ class Lead
 
         $json = json_decode($res->getBody(), 1);
 
-        if(json_last_error())
-            throw new \Exception('Bad JSON format');
+        if (json_last_error()) {
+            throw new Exception('Bad JSON format!');
+        }
 
         return $json;
     }
